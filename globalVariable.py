2433de4af2253878e142e506dd7de83aee5f55ba
Louis <louis.munier@epfl.ch>
2018-04-22 17:33:19 +0200

    update timer

--- globalVariable.py
+++
@@ -1,7 +0,0 @@
-#!/usr/local/lib/python3.5
-# -*- coding: utf-8 -*-
-#lm220418.1230
-
-# This file contains all the global variables we need to pass in other modules
-
-dict = {"state":"stop"}
--- timer.py
+++ timer.py
@@ -8,7 +8,7 @@ import communication2gc as comm
 import time
 from threading import Timer
 
-beginning = 0.0
+beginning = 0.00
 
 def init(client):
 	client.subscribe("GC_2018/timer_in")
@@ -17,18 +17,21 @@ def timer(client, v_state):
 	global beginning
 
 	if v_state.value == 0 and beginning != 0.0:
-		beginning = 0.0
-		client.publish("GC_2018/timer_in", str(beginning))
+		beginning = 0.00
+		client.publish("GC_2018/timer_in", '{}:{:02}.{:02}'.format(0, 0, 0))
 
 	while v_state.value == 1:
-		time.sleep(0.1)
-		beginning += 0.1
+		time.sleep(0.01)
+		beginning += 0.01
 
 		if v_state.value == 0:
-			beginning = 0.0
+			beginning = 0.00
 
-		client.publish("GC_2018/timer_in", str(beginning))
+		min = int(beginning/60)
+		sec = int(beginning%60)
+		milli = int((beginning - int(beginning))*100)
 
+		client.publish("GC_2018/timer_in", '{}:{:02}.{:02}'.format(min, sec, milli))
 
 def main_timer(v_state):
 	client = comm.setup()
